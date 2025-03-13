6b2d2b0be77064fbd4e74c6dc02622b65e26ce35
wildfireOfMine <iamtheanguera@gmail.com>
2025-03-13 22:36:18 +0100

    Added .gitignore. Added an API on the SPA

--- src/page/SPA.jsx
+++ src/page/SPA.jsx
@@ -1,12 +1,18 @@
-import { Button, Slide } from '@mui/material'
+import { Button, Link, Slide } from '@mui/material'
 import React from 'react'
 import SendIcon from '@mui/icons-material/Send';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { LinkedIn } from '@mui/icons-material';
+import HtmlIcon from '@mui/icons-material/Html';
+import CssIcon from '@mui/icons-material/Css';
+import JavascriptIcon from '@mui/icons-material/Javascript';
 
 const SPA = () => {
+  const api_key = import.meta.env.VITE_API_KEY
+
   return (
     <>
+        <h2></h2>
         <h2>Welcome...</h2>
         <h1>My Odin Path</h1>
         <ul>
@@ -24,11 +30,31 @@ const SPA = () => {
 
         {/* Do the list of it basically */}
         <ul>
-          <li>Receips</li>
-          <li>Landing Page</li>
-          <li>Rock, Papers, Scissors</li>
-          <li>Etch-a-Sketch</li>
-          <li>Calculator</li>
+          <li>Receips  <HtmlIcon/> 
+            <Link href="https://wildfireofmine.github.io/odin-recipes/">
+              <img src={`https://api.apiflash.com/v1/urltoimage?access_key=${api_key}&url=https%3A%2F%2Fwildfireofmine.github.io%2Fodin-recipes%2F&format=png`} />
+            </Link>
+          </li>
+          <li>Landing Page <CssIcon/> 
+            <Link href="https://wildfireofmine.github.io/odin-landing-page/">
+              <img src={`https://api.apiflash.com/v1/urltoimage?access_key=${api_key}&url=https%3A%2F%2Fwildfireofmine.github.io%2Fodin-landing-page%2F&format=png`} />
+            </Link>
+          </li>
+          <li>Rock, Papers, Scissors <JavascriptIcon/> 
+            <Link href="https://wildfireofmine.github.io/Rock-Papers-Scissors/">
+              <img src={`https://api.apiflash.com/v1/urltoimage?access_key=${api_key}&url=https%3A%2F%2Fwildfireofmine.github.io%2FRock-Papers-Scissors%2F&format=png`} />
+            </Link>
+          </li>
+          <li>Etch-a-Sketch <HtmlIcon/> <CssIcon/> <JavascriptIcon/> 
+            <Link href="https://wildfireofmine.github.io/etchASketch/">
+              <img src={`https://api.apiflash.com/v1/urltoimage?access_key=${api_key}&url=https%3A%2F%2Fwildfireofmine.github.io%2FetchASketch%2F&format=png`} />
+            </Link>
+          </li>
+          <li>Calculator <HtmlIcon/> <CssIcon/> <JavascriptIcon/> 
+            <Link href="https://wildfireofmine.github.io/Calculator/">
+              <img src={`https://api.apiflash.com/v1/urltoimage?access_key=${api_key}&url=https%3A%2F%2Fwildfireofmine.github.io%2FetchASketch%2Fhttps%3A%2F%2Fwildfireofmine.github.io%2FCalculator%2F&format=png`} />
+            </Link>
+          </li>
           <li>CV Application</li>
           <li>Memory Card</li>
         </ul>
