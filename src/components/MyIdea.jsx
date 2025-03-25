a560d7a3e314e0cc2d0efb677bada85d7bcd0eaf
wildfireOfMine <iamtheanguera@gmail.com>
2025-03-25 12:56:26 +0100

    Modified some info

--- src/components/MyIdea.jsx
+++ src/components/MyIdea.jsx
@@ -8,7 +8,7 @@ const MyIdea = () => {
           <li>What is This?
             <h2>This is a SPA (Single Page Application) created with the following aims:</h2>
             <ul>
-              <li>Gather and collect all my TheOdinProject projects and tasks in a SPA</li>
+              <li>Gather and collect all my TheOdinProject projects and tasks so far done in a SPA</li>
               <li>Test and practise with React's MUI library</li>
             </ul>
           </li>
