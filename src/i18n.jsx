4eca7080e519938204816c91663476d1c004e74f
wildfireOfMine <iamtheanguera@gmail.com>
2025-03-27 14:48:49 +0100

    Added full translation for Catalan

--- src/i18n.jsx
+++ src/i18n.jsx
@@ -3,13 +3,16 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import * as common_en from '../src/locales/en/translation.json'
 import * as common_es from '../src/locales/es/translation.json'
+import * as common_cat from '../src/locales/cat/translation.json'
+import * as common_pr from '../src/locales/pr/translation.json'
+import * as common_it from '../src/locales/it/translation.json'
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    supportedLngs: ['en', 'es'],
-    fallbackLng: 'es',
+    supportedLngs: ['en', 'es', 'cat', 'pr', 'it'],
+    fallbackLng: 'en',
     resources: {
       en: {
         translation: common_en,
@@ -17,10 +20,19 @@ i18n
       es: {
         translation: common_es,
       },
+      cat: {
+        translation: common_cat,
+      },
+      pr: {
+        translation: common_pr,
+      },
+      it: {
+        translation: common_it,
+      },
     },
     ns: ["translation"],
     interpolation: { escapeValue: false },
-    debug: true,
+    debug: false,
     react: {
       useSuspense: false,
       bindI18n: 'languageChanged loaded',
